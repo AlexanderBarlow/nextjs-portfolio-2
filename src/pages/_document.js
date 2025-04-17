3a6acafe2f0683350cb989bf895d79094cd8b6a9
Alexander Barlow <alex.barlow@rocketmail.com>
2025-04-16 21:43:11 -0400

    pwa/ icon support

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -29,15 +29,31 @@ export default class MyDocument extends Document {
   }
   render() {
     return (
-      <Html lang='en-GB'>
-        <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+			<Html lang="en-GB">
+				<Head>
+					<link rel="manifest" href="/manifest.json" />
+					<meta name="theme-color" content="#111111" />
+					<link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+					<meta name="apple-mobile-web-app-capable" content="yes" />
+					<meta
+						name="apple-mobile-web-app-status-bar-style"
+						content="black-translucent"
+					/>
+					<meta name="mobile-web-app-capable" content="yes" />
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
   }
 }
